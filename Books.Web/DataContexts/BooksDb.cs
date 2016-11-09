9a5c58064609061a986b00939f6c16a3bf97e829
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-11-10 01:17:57 +0200

    EF 6. Add two project (Books.Web, Books.Entities)

---
+++ Books.Web/DataContexts/BooksDb.cs
@@ -0,0 +1,10 @@
+﻿using Books.Entities;
+using System.Data.Entity;
+
+namespace Books.Web.DataContexts
+{
+    public class BooksDb : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
